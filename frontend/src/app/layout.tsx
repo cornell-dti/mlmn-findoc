8a765f1104a65c9ecbf9ca60c8843d841e3b7cc1
Pratyush Sudhakar <ps2245@cornell.edu>
2024-02-29 00:14:06 -0500

    add dti logo

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -8,6 +8,7 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "DTI GPT",
   description: "",
+  icons: "/favicon.ico",
 };
 
 export default function RootLayout({
