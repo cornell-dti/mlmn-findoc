11b917e00aae889aef9e74bf5ea43a96f5f6f7db
Oscar Wang <oscarwang22396@gmail.com>
2024-04-24 14:24:06 -0400

    supabase

--- backend/supabase_client.py
+++ backend/supabase_client.py
@@ -21,14 +21,14 @@ def get_user_by_email(email: str):
 def upload_doc(user_id: str, doc_text: str):
     return supabase_client.from_("user-doc").insert({"userID": user_id, "docID": doc_text}).execute()
 
-def main():
-    data, _ = get_user_by_email("ow39@cornell.edu")
-    # USERS.delete().neq("first_name", 0).execute()
-    # DOCS.insert({"user_id": "5", "doc_text": "Hello, world!"}).execute()
-    uid = data[1][0]["id"]
-    # DOCS.insert({"userID": uid, "docID": 123456}).execute()
-    # QUERIES.insert({"userID": uid, "queryID": 123456}).execute()
-    print(get_docs_by_user(uid))
+# def main():
+#     data, _ = get_user_by_email("ow39@cornell.edu")
+#     # USERS.delete().neq("first_name", 0).execute()
+#     # DOCS.insert({"user_id": "5", "doc_text": "Hello, world!"}).execute()
+#     uid = data[1][0]["id"]
+#     # DOCS.insert({"userID": uid, "docID": 123456}).execute()
+#     # QUERIES.insert({"userID": uid, "queryID": 123456}).execute()
+#     print(get_docs_by_user(uid))
 
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()
