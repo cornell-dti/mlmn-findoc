91b280b9d795ada6b2ebbebaca35700169f10c53
Pratyush Sudhakar <ps2245@cornell.edu>
2024-04-24 21:34:37 -0400

    clean up

--- backend/server.py
+++ backend/server.py
@@ -140,7 +140,6 @@ def callback():
         "client_secret": credentials.client_secret,
         "scopes": credentials.scopes,
     }
-    print(session["credentials"])
     return render_template_string(
         """
         <html>
