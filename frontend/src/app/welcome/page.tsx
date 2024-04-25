5eb9bf2e1cd6a423cf1038d8fe422ab2b4b12aa7
Alyssa Zhang <alyssayzhang@gmail.com>
2024-04-24 21:44:29 -0400

    welcome page styling

--- frontend/src/app/welcome/page.tsx
+++ frontend/src/app/welcome/page.tsx
@@ -13,13 +13,14 @@ export default function Home() {
      <div className="min-h-screen bg-mainContent">
 
       <div className="w-full px-52 pt-52">
-          <p className="text-left text-5xl  font-medium text-white">Welcome, {session?.user?.name}!</p>
-          <p className="text-left text-5xl  font-medium text-white">What can we help you with?</p>
+          <p className="text-left text-5xl font-normal text-white">Welcome, {session?.user?.name}!</p>
+          <p className="mb-2"> </p>
+          <p className="text-left text-5xl font-normal text-white">What can we help you with?</p>
       </div>
 
       <div className="flex justify-normal mt-20 space-x-12 w-full px-52">
             <Link href="/chat/summarize">
-              <button className="w-80 h-32 rounded-md shadow bg-buttonColor hover:bg-hoverColor">
+              <button className="w-80 h-32 rounded-lg shadow bg-buttonColor hover:bg-hoverColor">
                 <span className="w-full h-full flex flex-col justify-center items-left text-white text-left text-lg font-medium px-12">
                   Summarize
                   <span className="text-sm">to get important info</span>
@@ -29,7 +30,7 @@ export default function Home() {
 
 
         <Link href="/chat/parse">
-        <button className="w-80 h-32 rounded-md shadow bg-buttonColor hover:bg-hoverColor">
+        <button className="w-80 h-32 rounded-lg shadow bg-buttonColor hover:bg-hoverColor">
                 <span className="w-full h-full flex flex-col justify-center items-left text-white text-left text-lg font-medium px-12">
                     Parse
                     <span className="text-sm">from syllabus</span>
@@ -38,7 +39,7 @@ export default function Home() {
             </Link>
 
         <Link href="/chat/compare">
-        <button className="w-80 h-32 rounded-md shadow bg-buttonColor hover:bg-hoverColor">
+        <button className="w-88 h-32 rounded-md shadow bg-buttonColor hover:bg-hoverColor">
                 <span className="w-full h-full flex flex-col justify-center items-left text-white text-left text-lg font-medium px-12">
                     Compare with another file
                     <span className="text-sm">from financial reports & documents</span>
